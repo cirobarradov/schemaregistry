dd755bb99f3e35574b210d6f1685215df3936bfe
Ricardo Bravo <rbravo79@gmail.com>
2020-07-24 08:33:04 +0200

    Update GpsFake.java

--- src/main/java/org/azure/schema/GpsFake.java
+++ src/main/java/org/azure/schema/GpsFake.java
@@ -25,10 +25,11 @@ public class GpsFake {
     public GpsFake ()
     {
         try {
+            //cache input1.json schema associated to topic1
             updateInternalReg(TOPIC_1, "input1.json");
-
+            //cache input2.json schema associated to topic1
             updateInternalReg(TOPIC_1, "input2.json");
-
+            //cache input3.json schema associated to topic2
             updateInternalReg(TOPIC_2, "input3.json");
 
         } catch (IOException e) {
@@ -37,14 +38,15 @@ public class GpsFake {
     }
 
     public void updateInternalReg(String topic, String jsonPath ) throws IOException {
-
+        //get json content from filepath
         String json = IOUtils.toString(MainTest.class.getClassLoader().getResourceAsStream(jsonPath), StandardCharsets.UTF_8.name());
+        //get avro schema associated to json
         String schema = new AvroConverter().convert(json);
-
+        //store schema in schema registry
         JsonNode response =registry.postSubject(topic+"-value",schema);
-
+        //get Id from schema registry
         Integer id=jsonConverter.getId(response);
-
+        //create tuple with id and topic
         gpsRegistry.put(jsonPath,new Tuple2(id,topic));
     }
 
@@ -59,8 +61,11 @@ public class GpsFake {
                     while (it.hasNext())
                     {
                         try {
+                            // key = filename
                             String key = (String)it.next();
+                            // json = filename content
                             String json = IOUtils.toString(MainTest.class.getClassLoader().getResourceAsStream(key), StandardCharsets.UTF_8.name());
+                            // sendIdSchema(String topic, String value,  String id)
                             producer.sendIdSchema(gpsRegistry.get(key)._2, json, gpsRegistry.get(key)._1.toString());
                         } catch (IOException e) {
                             e.printStackTrace();
